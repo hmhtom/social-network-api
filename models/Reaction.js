42b945a6d5ebacf08fe672f34f74fb7398791abe
hmhtom <hmhtom@gmail.com>
2022-11-14 20:59:22 -0500

    Add _id false to reaction schema

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,25 +1,33 @@
 const { Schema, Types } = require("mongoose");
 const format = require("date-format");
 
-const reactionSchema = new Schema({
-  reactionId: {
-    type: Schema.Types.ObjectId,
-    default: () => new Types.ObjectId(),
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: (date) => format.asString("yyyy/MM/dd hh:mm:ss", date),
+    },
   },
-  reactionBody: {
-    type: String,
-    required: true,
-    maxlength: 280,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    get: (date) => format.asString("yyyy/MM/dd hh:mm:ss", date),
-  },
-});
+  {
+    toJSON: {
+      getters: true,
+    },
+    _id: false,
+  }
+);
 
 module.exports = reactionSchema;
